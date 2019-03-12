a0c3bdfa6206fdadedbe66c31de8ccbc83865706
jsincavage <jsincavage@lingraphica.com>
2019-03-12 11:54:10 -0400

    first commit

---
+++ lib/lingraphicaSass.rb
@@ -0,0 +1,9 @@
+require "lingraphicaSass/version"
+
+module LingraphicaSass
+  class Error < StandardError; end
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+   end
+end
---
+++ lib/lingraphicaSass/version.rb
@@ -0,0 +1,3 @@
+module LingraphicaSass
+  VERSION = "1.0.0"
+end
